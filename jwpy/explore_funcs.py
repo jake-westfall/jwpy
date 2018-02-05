c4790753a992686e40d79b8aaceae675548d498a
Jake <jake@Jakes-iMac.local>
2018-02-05 11:34:14 -0600

    Add HCUP processing function + test

--- jwpy/explore_funcs.py
+++ jwpy/explore_funcs.py
@@ -15,14 +15,14 @@ def summarize_df(df, head=5, dropna=False):
         head:
         dropna:
     '''
-    
+
     # show variable types and numbers of unique levels
     summ = pd.DataFrame(
         [df.dtypes, df.nunique(dropna=dropna), df.isnull().mean(0)],
-        index=['dtype','nunique','%missing'])
+        index=['dtype', 'nunique', '%missing'])
     # append head
     summ = pd.concat([summ, df.head(head)], axis=0)
-    
+
     print('{} rows x {} columns'.format(*df.shape))
     return summ
 
@@ -54,16 +54,61 @@ def aov_xtab(values, index, columns, figsize=(13, 8), **kwargs):
     dat = pd.DataFrame({
         'values': values,
         'index': index,
-        'columns': columns
-        })
+        'columns': columns})
 
     # fit the beta-binomial model
     endog = pd.DataFrame({
-        'k': dat.groupby(['index','columns'])['values'].sum(),
-        'n': dat.groupby(['index','columns'])['values'].count()
-        })
+        'k': dat.groupby(['index', 'columns'])['values'].sum(),
+        'n': dat.groupby(['index', 'columns'])['values'].count()})
     a, b = betabinom(endog=endog).fit(**kwargs).params
-    m = a/(a+b)
+    m = a / (a + b)
+
+    # compute the shrunken cell means and sort the matrix by row/column means
+    shrunk = pd.pivot_table(dat, values='values', index='index',
+                            columns='columns',
+                            aggfunc=partial(shrink, a=a, b=b), fill_value=m)
+    new_index = shrunk.index[shrunk.mean(1).argsort()[::-1]]
+    new_columns = shrunk.columns[shrunk.mean(0).argsort()[::-1]]
+    shrunk = shrunk.reindex_axis(new_index, axis=0)
+    shrunk = shrunk.reindex_axis(new_columns, axis=1)
+
+    # compute the numbers of obs. per cell and sort by the same means as above
+    annot = pd.crosstab(dat['index'], dat['columns'])
+    annot = annot.reindex_axis(new_index, axis=0)
+    annot = annot.reindex_axis(new_columns, axis=1)
+
+    # print stuff
+    print('SD of row means: {}'.format(shrunk.mean(1).std()))
+    print('SD of column means: {}'.format(shrunk.mean(0).std()))
+    interaction = shrunk.sub(shrunk.mean(1), axis=0).sub(shrunk.mean(0), axis=1)
+    print('SD of row*column interaction: {}'.format(interaction.stack().std()))
+
+    # plot!
+    fig, ax = plt.subplots(figsize=figsize)
+    return sns.heatmap(shrunk, annot=annot, fmt='.0f');
+
+
+def shrink_1d(target, group, sort=True, **kwargs):
+    '''
+    Exploratory plot for pairs of categorical predictors/features.
+    Args:
+        target: binary (integer or boolean) DV
+        group: grouping factor
+        sort:
+        kwargs:
+    '''
+
+    # group the variables into a DataFrame
+    dat = pd.DataFrame({
+        'target': target,
+        'group': group})
+
+    # fit the beta-binomial model
+    endog = pd.DataFrame({
+        'k': dat.groupby('group')['target'].sum(),
+        'n': dat.groupby('group')['target'].count()})
+    a, b = betabinom(endog=endog).fit(**kwargs).params
+    m = a / (a + b)
 
     # compute the shrunken cell means and sort the matrix by row/column means
     shrunk = pd.pivot_table(dat, values='values', index='index',
@@ -87,4 +132,3 @@ def aov_xtab(values, index, columns, figsize=(13, 8), **kwargs):
     # plot!
     fig, ax = plt.subplots(figsize=figsize)
     return sns.heatmap(shrunk, annot=annot, fmt='.0f');
-
---
+++ jwpy/sas_fwf.py
@@ -0,0 +1,90 @@
+import numpy as np
+import pandas as pd
+import os
+import re
+from jwpy.explore_funcs import summarize_df
+import pyhcup
+
+sas_path = '/Users/jake/Desktop/Google Drive/jwpy/jwpy/'
+
+os.getcwd()
+# os.chdir(os.path.dirname(sas_path))
+
+
+def read_hcup(data_file, sas_script, chunksize=500000, combine_chunks=True,
+              return_meta=False):
+    '''
+    Arguments:
+        data_file (str): Path of fixed-width text data file
+        sas_script (str): Path of the accompanying SAS load file
+        chunksize (int): Break data_file into chunks with nrows=chunksize and
+            read/process each chunk separately (for lower memory usage)
+        combine_chunks (bool): Return single DataFrame with all chunks combined
+            (True; default), or return list of DataFrame chunks (False)
+        return_meta (bool): Return the data + a DataFrame of column metadata
+            (True), or just return the processed data (False; default)
+
+    Returns:
+        Default: a single pandas DataFrame
+        If combine_chunks=False: List of pandas DataFrames
+        If return_meta=True: Tuple with data in first element and metadata in
+            second element
+    '''
+    # read in the sas script
+    with open(sas_script) as f:
+        sas = f.readlines()
+
+    # grab the lines that define the fields
+    fields = [re.search(r'@\s*(\d+)\s+(\S+)\s+(\S+)\s?', x) for x in sas]
+    fields = [x.groups() for x in fields if x]
+
+    # from those, grab the names and starting positions, and infer the dtypes
+    starts = [int(x[0]) for x in fields]
+    names = [x[1] for x in fields]
+    dtype = ['category' if re.search(r'CHAR', x[2]) else float for x in fields]
+    # must use floats, since int columns can't have missing values
+    # because it's okay because floats hardly use more space than ints
+
+    # convert dtype list into dictionary (for pd.read_fwf)
+    dtype = {name: dt for name, dt in zip(names, dtype)}
+
+    # compute the variable widths
+    maxcols = int(re.search(r'LRECL = (.+);', ''.join(sas)).group(1))
+    widths = np.diff(starts + [maxcols+1])
+
+    # grab all the missing value codes
+    na_vals = re.findall(r'\'(.+)\' = \S+', ''.join(sas))
+    na_vals += ['.']
+
+    # read the data from disk in bite-sized chunks
+    dat = pd.read_fwf(data_file, header=None, names=names, widths=widths,
+                      dtype=dtype, na_values=na_vals, chunksize=chunksize)
+    # convert generator to list
+    dat = list(dat)
+
+    if len(dat) > 1:
+        # preserve categories instead of converting to objects.
+        # if you concat categories w/ different levels (or even the same in a
+        # different order), it silently converts to object
+        levels = {col: set() for col in dtype.keys()}
+        for col, dt in dtype.items():
+            if dt == 'category':
+                for d in dat:
+                    levels[col] = set.union(levels[col], d[col].cat.categories)
+                for d in dat:
+                    _newlevels = list(levels[col] - set(d[col].cat.categories))
+                    d[col] = d[col].cat.add_categories(_newlevels)
+                    d[col] = d[col].cat.reorder_categories(levels[col])
+
+    # recombine the chunks and return the result
+    dat = pd.concat(dat)
+    return dat
+
+
+files = ['NIS_2015_Core', 'NIS_2015_Hospital', 'NIS_2015Q1Q3_DX_PR_GRPS',
+         'NIS_2015Q4_DX_PR_GRPS', 'NIS_2015Q1Q3_Severity',
+         'NIS_2015Q4_Severity']
+datasets = [read_hcup(data_file='./fwf_test/'+f+'.fwf',
+                      sas_script='./fwf_test/SASLoad_'+f+'.SAS',
+                      chunksize=10) for f in files]
+summarize_df(datasets[4])
