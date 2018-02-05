50914fc6b8784017a3d0d458bda60622e1309388
Jake <jake@Jakes-iMac.local>
2018-02-05 11:38:39 -0600

    Revert "Add HCUP processing function + test"
    This reverts commit c4790753a992686e40d79b8aaceae675548d498a.

--- tests/test_jwpy.py
+++ tests/test_jwpy.py
@@ -5,8 +5,8 @@
 
 import pytest
 
-from jwpy.explore_funcs import summarize_df
-from jwpy.sas_fwf import head_hcup
+
+from jwpy import jwpy
 
 
 @pytest.fixture
@@ -23,12 +23,3 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-def test_read_hcup():
-    files = ['NIS_2015_Core', 'NIS_2015_Hospital', 'NIS_2015Q1Q3_DX_PR_GRPS',
-             'NIS_2015Q4_DX_PR_GRPS', 'NIS_2015Q1Q3_Severity',
-             'NIS_2015Q4_Severity']
-    datasets = [read_hcup(data_file='./fwf_test/'+f+'.fwf',
-                          sas_script='./fwf_test/SASLoad_'+f+'.SAS',
-                          chunksize=10) for f in files]
-    summarize_df(datasets[4])
