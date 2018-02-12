7755a8276e24d896992dfe65bc94b9d275c769cf
jake-westfall <jake.a.westfall@gmail.com>
2018-02-12 10:01:03 -0600

    Add align_cols(); Expand Timer docstring

--- jwpy/misc.py
+++ jwpy/misc.py
@@ -21,12 +21,14 @@ os.getcwd()
 
 class Timer:
     '''
-    Usage:
+    Similar to the `%%time` magic, but it doesn't have to be run at the top of
+    the cell, and the indentation makes it a little nicer visually. Usage:
+    ```
     with Timer():
         result = cool_stuff()
-
-    Note that commands that in the with block will only print output if print()
-    is explicitly called on the result
+    ```
+    Note that commands in the with block will only print output if print() is
+    explicitly called on the result
 
     Code ripped from:
     http://code.activestate.com/recipes/577896-benchmark-code-with-the-with-statement/
@@ -51,3 +53,43 @@ class Timer:
         self.interval = self.end - self.start
         if self.verbose:
             print('time taken: %f seconds' % self.interval)
+
+
+def align_cols(df1, df2):
+    '''
+    Wrapper for pandas.DataFrame.align(..., axis=1) that better handles dtypes.
+
+    Args:
+        df1, df1: The two dataframes to align column-wise, fill in missing
+            values, etc.
+
+    Returns:
+        A tuple containing (df1_aligned, df2_aligned).
+    '''
+    # align using pandas
+    p1, p2 = df1.align(df2, axis=1)
+    # define empty sets to hold the categories of each categorical
+    levels = {col: set() for col in p1.columns}
+    for col in levels.keys():
+        # preserve categories
+        check = str(p1[col].dtype) == 'category', str(p2[col].dtype) == 'category'
+        if check[0] | check[1]:
+            # make sure they're BOTH categoricals first
+            if xor(check[0], check[1]):
+                if check.index(True):
+                    p1[col] = p1[col].astype('category')
+                else:
+                    p2[col] = p2[col].astype('category')
+            # make sure both categoricals have the same categories
+            for d in (p1, p2):
+                levels[col] = set.union(levels[col], d[col].cat.categories)
+            for d in (p1, p2):
+                _newlevels = list(levels[col] - set(d[col].cat.categories))
+                d[col] = d[col].cat.add_categories(_newlevels)
+                d[col] = d[col].cat.reorder_categories(levels[col])
+        # downcast float64 to float32
+        else:
+            for d in (p1, p2):
+                if str(d[col].dtype) == 'float64':
+                    d[col] = pd.to_numeric(d[col], downcast='float')
+    return p1, p2
